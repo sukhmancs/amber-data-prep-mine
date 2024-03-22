983f32130f6388c4a427a20ac8e7a9c1a95e3276
racoot <sukhmanghum66778899@gmail.com>
2024-03-22 16:37:27 -0400

    added: data

--- redpajama_v1/download.sh
+++ redpajama_v1/download.sh
@@ -2,5 +2,5 @@
 while read line; do
     dload_loc=${line#https://data.together.xyz/redpajama-data-1T/v1.0.0/}
     mkdir -p $(dirname $dload_loc)
-    wget "$line" -O "$dload_loc"
+    wget -nc "$line" -O "$dload_loc" 
 done < urls.txt
