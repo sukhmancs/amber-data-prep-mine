96c9ba42fcb5186de6c2935bbaf6a8f1e9c574d9
racoot <sukhmanghum66778899@gmail.com>
2024-03-27 18:08:38 -0400

    added: files

--- redpajama_v1/download.sh
+++ redpajama_v1/download.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+
+num_lines=$1
+
+# check $num_lines is set
+if [[ -z "$num_lines" ]]; then
+    echo "No argument provided. Please provide the number of URLs to download."
+    exit 1
+fi
+
+count=0
 # wget 'https://data.together.xyz/redpajama-data-1T/v1.0.0/urls.txt'
 while read line; do
+    if (( count >= num_lines )); then
+        break
+    fi
+    
     dload_loc=${line#https://data.together.xyz/redpajama-data-1T/v1.0.0/}
     mkdir -p $(dirname $dload_loc)
-    wget -nc "$line" -O "$dload_loc" 
+    wget -nc "$line" -O "$dload_loc"
+    
+    ((count++))
 done < urls.txt
